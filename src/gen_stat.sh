516d52b30ff196f6d94ad3b820fef47fdaaa21e9
Aaron SU <aaron.su0424@gmail.com>
2024-01-22 09:51:07 +0100

    update gen stat script

--- src/gen_stat.sh
+++ src/gen_stat.sh
@@ -7,25 +7,50 @@ CLANG_CFLAGS="-march=native -g3 -fopenmp=libgomp -lm -I/usr/lib/gcc/x86_64-linux
 GCC_CFLAGS="-march=native -g3 -fopenmp -funroll-loops"
 
 echo "gcc_o2"
-gcc $GCC_CFLAGS -O2 nbody5.c -o bin/gcc_O2_nbody -lm && bin/gcc_O2_nbody > data/gcc_O2_nbody
-cat data/gcc_O2_nbody
+gcc $GCC_CFLAGS -O2 nbody5.c -o bin/gcc_O2_nbody -lm && taskset -c 12 bin/gcc_O2_nbody > data/seq_gcc_O2_nbody
+cat data/seq_gcc_O2_nbody
 
 echo "gcc_o3"
-gcc $GCC_CFLAGS -O3 nbody5.c -o bin/gcc_O3_nbody -lm && bin/gcc_O3_nbody > data/gcc_O3_nbody 
-cat data/gcc_O3_nbody
+gcc $GCC_CFLAGS -O3 nbody5.c -o bin/gcc_O3_nbody -lm && taskset -c 12 bin/gcc_O3_nbody > data/seq_gcc_O3_nbody 
+cat data/seq_gcc_O3_nbody
 
 echo "gcc_ofast"
-gcc $GCC_CFLAGS -Ofast nbody5.c -o bin/gcc_Ofast_nbody -lm && bin/gcc_Ofast_nbody > data/gcc_Ofast_nbody
-cat data/gcc_Ofast_nbody
+gcc $GCC_CFLAGS -Ofast nbody5.c -o bin/gcc_Ofast_nbody -lm && taskset -c 12 bin/gcc_Ofast_nbody > data/seq_gcc_Ofast_nbody
+cat data/seq_gcc_Ofast_nbody
 
 echo "clang_o2"
-clang $CLANG_CFLAGS -O2 nbody5.c -o bin/clang_O2_nbody -lm && bin/clang_O2_nbody > data/clang_O2_nbody
-cat data/clang_O2_nbody
+clang $CLANG_CFLAGS -O2 nbody5.c -o bin/clang_O2_nbody -lm && taskset -c 12 bin/clang_O2_nbody > data/seq_clang_O2_nbody
+cat data/seq_clang_O2_nbody
 
 echo "clang_o3"
-clang $CLANG_CFLAGS -O3 nbody5.c -o bin/clang_O3_nbody -lm && bin/clang_O3_nbody > data/clang_O3_nbody
-cat data/clang_O3_nbody
+clang $CLANG_CFLAGS -O3 nbody5.c -o bin/clang_O3_nbody -lm && taskset -c 12 bin/clang_O3_nbody > data/seq_clang_O3_nbody
+cat data/seq_clang_O3_nbody
 
 echo "clang_ofast"
-clang $CLANG_CFLAGS -Ofast nbody5.c -o bin/clang_Ofast_nbody -lm && bin/clang_Ofast_nbody > data/clang_Ofast_nbody
-cat data/clang_Ofast_nbody
+clang $CLANG_CFLAGS -Ofast nbody5.c -o bin/clang_Ofast_nbody -lm && taskset -c 12 bin/clang_Ofast_nbody > data/seq_clang_Ofast_nbody
+cat data/seq_clang_Ofast_nbody
+
+
+echo "gcc_o2"
+gcc $GCC_CFLAGS -O2 nbody5.c -o bin/gcc_O2_nbody -lm && bin/gcc_O2_nbody > data/seqpar_gcc_O2_nbody
+cat data/par_gcc_O2_nbody
+
+echo "gcc_o3"
+gcc $GCC_CFLAGS -O3 nbody5.c -o bin/gcc_O3_nbody -lm && bin/gcc_O3_nbody > data/par_gcc_O3_nbody 
+cat data/par_gcc_O3_nbody
+
+echo "gcc_ofast"
+gcc $GCC_CFLAGS -Ofast nbody5.c -o bin/gcc_Ofast_nbody -lm && bin/gcc_Ofast_nbody > data/par_gcc_Ofast_nbody
+cat data/par_gcc_Ofast_nbody
+
+echo "clang_o2"
+clang $CLANG_CFLAGS -O2 nbody5.c -o bin/clang_O2_nbody -lm && bin/clang_O2_nbody > data/par_clang_O2_nbody
+cat data/par_clang_O2_nbody
+
+echo "clang_o3"
+clang $CLANG_CFLAGS -O3 nbody5.c -o bin/clang_O3_nbody -lm && bin/clang_O3_nbody > data/par_clang_O3_nbody
+cat data/par_clang_O3_nbody
+
+echo "clang_ofast"
+clang $CLANG_CFLAGS -Ofast nbody5.c -o bin/clang_Ofast_nbody -lm && bin/clang_Ofast_nbody > data/par_clang_Ofast_nbody
+cat data/par_clang_Ofast_nbody
